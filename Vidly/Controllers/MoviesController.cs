f0fac166695ef9a4bed8fc49441820c855d0c9f0
olayeni45 <olayeni45@gmail.com>
2023-02-22 12:32:10 +0100

    feat: Movies controller, model and view.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Name = "Shrek" };
+
+            return View(movie);
+        }
+    }
+}
