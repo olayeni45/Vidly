77e37d768256a97fa4bce74afd650129d83bb701
olayeni45 <olayeni45@gmail.com>
2023-02-22 21:06:41 +0100

    feat: view model

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -10,7 +12,50 @@ namespace Vidly.Controllers
         {
             Movie movie = new Movie() { Name = "Shrek" };
 
-            return View(movie);
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer{ Name="Customer 1"},
+                new Customer {Name="Customer 2"}
+            };
+
+            RandomMovieViewModel viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            //ViewData["movie"] = movie;
+            //ViewBag.Movie = movie;
+
+            return View(viewModel);
+        }
+
+        //GET: /movies/edit/1 OR /movies/edit?id=1
+        public ActionResult Edit(int id)
+        {
+            return Content("Id = " + id);
+        }
+
+        //GET: /movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+
+        }
+
+
+        //GET: /movies/release/{year}/{month}
+        //Route attribute: [Route()]
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
