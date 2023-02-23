2606df9bea05905970e7fad93b9ccbafb424775c
olayeni45 <olayeni45@gmail.com>
2023-02-23 11:18:14 +0100

    feat: overriding customer class fields

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,10 +1,17 @@
-﻿namespace Vidly.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsLetter { get; set; }
+
         public MembershipType MembershipType { get; set; }
 
         //Entity framework recognizes this as the foreign key of the Class MembershipType
