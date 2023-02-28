32406cc56f0c274a62d765a1eab6e9c77bf36471
olayeni45 <olayeni45@gmail.com>
2023-02-28 10:22:27 +0100

    feat: seeding users and roles

--- Vidly/Controllers/AccountController.cs
+++ Vidly/Controllers/AccountController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Globalization;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
 using Vidly.Models;
 
 namespace Vidly.Controllers
@@ -22,7 +19,7 @@ namespace Vidly.Controllers
         {
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -34,9 +31,9 @@ namespace Vidly.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -120,7 +117,7 @@ namespace Vidly.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -155,8 +152,19 @@ namespace Vidly.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
+                    /*
+                     * Temp code
+                     * Creating a role 'CanManageMovies' to the admin
+                     * DO NOT UNCOMMENT
+                    var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    var roleManager = new RoleManager<IdentityRole>(roleStore);
+                    await roleManager.CreateAsync(new IdentityRole("CanManageMovies"));
+                    await UserManager.AddToRoleAsync(user.Id, "CanManageMovies");
+                    */
+
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
---
+++ Vidly/Migrations/202302280911185_SeedUsers.cs
@@ -0,0 +1,29 @@
+﻿namespace Vidly.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class SeedUsers : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"
+                    INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) 
+                    VALUES (N'2f19c9c8-d602-49de-99ab-33f7b38b6b4a', N'admin@vidly.com', 0, N'AMWzRwvkV6DZ/VxQxgI6K0t52oZgrrwWvMnfqsLsNZgfzoj82jsX3roeJR72YQxR0A==', N'845c85e3-ea41-4d52-bf58-431ec97fecae', NULL, 0, 0, NULL, 1, 0, N'admin@vidly.com')
+
+                    INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) 
+                    VALUES (N'eec84f2c-12ad-4d01-bd95-22b092559448', N'guest@vidly.com', 0, N'AIIDPe5Xa3vYJWDAqwV7ryaitSOlVP/H/LtkqkUc938S/duhZx6quwPo1lP8fXuUpQ==', N'51dc1e3d-0f7b-4010-bdb9-6f3522aa592e', NULL, 0, 0, NULL, 1, 0, N'guest@vidly.com')
+
+                    INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) 
+                    VALUES (N'febfc838-30e3-44ed-9c35-fc4985a7afb0', N'CanManageMovies')
+
+                    INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) 
+                    VALUES (N'2f19c9c8-d602-49de-99ab-33f7b38b6b4a', N'febfc838-30e3-44ed-9c35-fc4985a7afb0')
+
+                ");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
